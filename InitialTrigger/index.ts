edc82d4429981be20a4258fd42a546674b9e9a11
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2022-02-12 01:38:13 +0000

    IMPLEMENT InitialTrigger Azure function. Separate lint phase in CI/CD. PREP for Azure deployment.

---
+++ InitialTrigger/index.ts
@@ -0,0 +1,17 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import Response from "../models/Response/Response.js";
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('HTTP trigger function (v1/) processed a GET request.');
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: new Response(
+            200, 
+            new Date().toLocaleString(), 
+            '/api/v1/', 
+            { message: `cete-api routes up and running.` }
+        )
+    };
+};
+
+export default httpTrigger;
--- dist/test/test_models/Cete/test_Cete.js
+++ dist/test/test_models/Cete/test_Cete.js
@@ -1,14 +1,19 @@
+"use strict";
 /**
  * Cete Test Harness
  * Tests the behaviour of the Cete type and corresponding functions
  */
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 // import Cete from "../../../models/Cete/Cete"
-import assert from 'assert';
+const assert_1 = __importDefault(require("assert"));
 describe('CLASS Cete', function () {
     describe('#updateIDForCete()', function () {
         it('should return -1 when the id cannot be calculated', function () {
             // TODO
-            assert.equal(0, 0);
+            assert_1.default.equal(0, 0);
         });
     });
 });
@@ -16,7 +21,7 @@ describe('CLASS Cete', function () {
     describe('#updateFilePathForCete()', function () {
         it('should return -1 when the filepath where the audio is stored in cannot be generated', function () {
             // TODO
-            assert.equal(0, 0);
+            assert_1.default.equal(0, 0);
         });
     });
 });
