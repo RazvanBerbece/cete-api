bf45d3ccf790e216b11109cb8a38b084f7bf2bf4
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2022-03-26 16:34:43 +0000

    RENAME Triggers. CHANGE URIs in code. ADD 'prep' npm command.

--- GetCetesForProfileTrigger/index.ts
+++ GetCetesForProfileTrigger/index.ts
@@ -5,7 +5,7 @@ import STATUS_CODES from "../models/StatusCode/statuses.js";
 
 const httpTrigger: AzureFunction = async function (context: Context): Promise<void> {
 
-    context.log('HTTP trigger function (v1/get/profile/cetes) is processing a GET request.');
+    context.log('HTTP trigger function (v1/get/profile) is processing a GET request.');
 
     // Get query params
     const userId = context.req.query.userId;
@@ -17,7 +17,7 @@ const httpTrigger: AzureFunction = async function (context: Context): Promise<vo
             status: STATUS_CODES.CLIENT_INVALID_REQUEST_NO_UID_OR_PARAM,
             body: new Response(
                 new Date().toLocaleString(), 
-                'api/v1/get/profile/cetes', 
+                'api/v1/get/profile', 
                 { message: `InvalidRequestNoUIDOrVisibility : GET Request has no UID or visibility query parameter` }
             ),
             headers: {
@@ -40,7 +40,7 @@ const httpTrigger: AzureFunction = async function (context: Context): Promise<vo
                 status: STATUS_CODES.SERVER_GET_AUDIO_METADATA_FROM_UID_BLOB,
                 body: new Response(
                     new Date().toLocaleString(), 
-                    'api/v1/get/profile/cetes', 
+                    'api/v1/get/profile', 
                     { message: `ErrorGetProfileCetesFromUIDBlobs : ${cetesDownloadResult.message}. GET Request has downloaded no data.` }
                 ),
                 headers: {
@@ -53,7 +53,7 @@ const httpTrigger: AzureFunction = async function (context: Context): Promise<vo
                 status: STATUS_CODES.SUCCESS,
                 body: new Response(
                     new Date().toLocaleString(), 
-                    'api/v1/get/profile/cetes', 
+                    'api/v1/get/profile', 
                     { 
                         message: `Downloaded ${cetesDownloadResult.length} cetes for user ${userId}`,
                         data: cetesDownloadResult
